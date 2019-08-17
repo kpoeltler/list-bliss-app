78159325d01e62a2a03b1a53b2fa13dc4bab2a07
kpoeltler <kpoeltler@gmail.com>
2019-08-17 00:55:08 -0500

    display function logs in console

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ let todos = ['item1', 'items2', 'item3'];
 
 //display todos
 function displayTodos(){
-  console.log(todos);
+  console.log(`My Todos List ${todos.slice(0, 2).join(', ')} and ${todos[2]}.`);
 }
+
+// function addTodo(){
+
+// }
+
+// function changeTodo(){
+
+// }
+
+// function deleteTodo(){
+
+// }
+
+console.log(displayTodos());
